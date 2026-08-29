1bd19daedfe3880eab9b9e24839a1a04824f7738
round48z-sketch <round48z@gmail.com>
2026-08-30 04:49:32 +0900

    Add files via upload

--- content/site.ts
+++ content/site.ts
@@ -13,11 +13,8 @@ export function getSiteUrl() {
   if (process.env.NEXT_PUBLIC_SITE_URL) {
     return process.env.NEXT_PUBLIC_SITE_URL.replace(/\/$/, "");
   }
-  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  if (process.env.NODE_ENV !== "production") {
+    return "http://localhost:3000";
   }
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
-  return "http://localhost:3000";
+  return "https://r13-picks.com";
 }
